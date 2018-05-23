bbe83330c2f1cc06761750be8a4ea6189502878f
Piotr <piotrparkola@gmail.com>
2018-05-23 12:59:47 +0200

    Init commit

---
+++ src/main/java/pl/parkin9/Hang_out_Man/HangOutManApplication.java
@@ -0,0 +1,12 @@
+package pl.parkin9.Hang_out_Man;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangOutManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangOutManApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/parkin9/Hang_out_Man/HangOutManApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.parkin9.Hang_out_Man;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HangOutManApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
