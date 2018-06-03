2a41a94b537f118087e12f00e220851be654133a
Piotr <piotrparkola@gmail.com>
2018-06-03 16:27:07 +0200

    Fix a little bug

---
+++ src/main/java/pl/parkin9/Hang_out_Man/Model/FormObject.java
@@ -0,0 +1,15 @@
+package pl.parkin9.Hang_out_Man.Model;
+
+public class FormObject {
+
+    private String hitLetter;
+
+
+    public String getHitLetter() {
+        return hitLetter;
+    }
+
+    public void setHitLetter(String hitLetter) {
+        this.hitLetter = hitLetter;
+    }
+}
--- src/main/java/pl/parkin9/Hang_out_Man/controller/IndexController.java
+++ src/main/java/pl/parkin9/Hang_out_Man/controller/IndexController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import pl.parkin9.Hang_out_Man.Model.FormObject;
 import pl.parkin9.Hang_out_Man.service.CodewordService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -24,7 +26,7 @@ public class IndexController {
 //////////////////////////////////////////////////////////////////////////////////
 
     @GetMapping("/")
-    public ModelAndView showIndex(HttpServletRequest request) {
+    public ModelAndView showIndexGet() {
 
         ModelAndView modelAndView = new ModelAndView("index");
 
@@ -32,12 +34,20 @@ public class IndexController {
     }
 
     @PostMapping("/")
-    public ModelAndView showGame(HttpServletRequest request) throws IOException {
+    public ModelAndView showIndexPost(HttpServletRequest request) {
 
-        ModelAndView modelAndView = new ModelAndView("game");
+        ModelAndView modelAndView = new ModelAndView("redirect:game");
 
         String player1name = request.getParameter("player1name");
         String player2name = request.getParameter("player2name");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/game")
+    public ModelAndView showGame() throws IOException {
+
+        ModelAndView modelAndView = new ModelAndView("game");
 
         String codeword = codewordService.getRandCodeword();
         modelAndView.addObject("codeword", codeword);
